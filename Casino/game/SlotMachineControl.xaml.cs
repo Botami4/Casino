26745ee67e1b146fff583a614a1662dca5f1aa00
milan <milan@MILN>
2025-11-19 08:47:57 +0100

    Add project files.

---
+++ Casino/game/SlotMachineControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Casino.game
+{
+    /// <summary>
+    /// Interaction logic for SlotMachineControl.xaml
+    /// </summary>
+    public partial class SlotMachineControl : UserControl
+    {
+        public SlotMachineControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
